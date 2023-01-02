d7db56ca6964d088bbd3b3f5942069a364c7f937
uchihiro <tohchi4122@gmail.com>
2023-01-02 14:42:29 +0900

    メモをローカルストレージに保存できるようにする
    メモをローカルストレージに保存し、リロードをしてもメモデータが消えないようにしました。

--- src/components/MemoForm.vue
+++ src/components/MemoForm.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue";
-const memos = ref([]);
+const STORAGE_KEY = "memosData";
+const memos = ref(JSON.parse(localStorage.getItem(STORAGE_KEY)) || []);
 const body = ref(undefined);
 
 const addTodo = () => {
@@ -9,7 +10,7 @@ const addTodo = () => {
     body: body.value,
   });
   body.value = "";
-  console.log(memos.value);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(memos.value));
 };
 </script>
 
@@ -17,6 +18,5 @@ const addTodo = () => {
   <form @submit.prevent="addTodo">
     <textarea v-model="body" cols="30" rows="10"></textarea>
     <button type="submit">追加</button>
-    <button>戻る</button>
   </form>
 </template>
